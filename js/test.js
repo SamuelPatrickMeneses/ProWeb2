b9e1d41dc9680101b1c020cb6116e2395de67fc2
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2022-03-22 15:59:02 -0300

    at5 0.2

--- js/test.js
+++ js/test.js
@@ -1,4 +1,7 @@
 document.onload(function(){
     var at5 = document.getElementsByClassName(".at5")[0];
-    document.body.
+    document.onload = function (){
+        document.body.classList.toggle("paleta1");
+    }
+    
 });
