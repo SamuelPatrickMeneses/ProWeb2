a8db18073fa98061a02222816d6de845f3fbf213
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2022-03-29 12:11:00 -0300

    at5 2.0

--- js/atb.js
+++ js/atb.js
@@ -1,21 +1,23 @@
-
-document.onload = function(){
-    document.querySelectorAll(".bc input").forEach(function(e,i,a){
-        console.log('oi1')
-        e.addEventListener("change",function(event){
-            console.log('oi')
-            var src = event.currentTarget;
-            var bc = src.parentNode;
-            var bcs = bc.parentNode.querySelectorAll('.bc');
-            var index = bcs.indexOf(bc);
-            bcs.forEach(function(e,i,a){
-                if(i < index)
-                    e.querySelector('input').checked = true;
-                else if(i > index)
-                    e.querySelector('input').checked = false;
-            });
-            bc.parentNode.parentNode.value = (index + 1) + ""
-        })
+var inputs = [];
+function changeAt(event) {
+    var src = event.currentTarget;
+    var bc = src.parentNode;
+    var bcs = [];
+    bc.parentNode.querySelectorAll('.bc').forEach(function (e) { bcs.push(e); });
+    var index = bcs.indexOf(bc);
+    bcs.forEach(function (e, i) {
+        if (i > index)
+            e.querySelector('input').checked = true;
+        else if (i < index)
+            e.querySelector('input').checked = false;
     });
-
+    var at = bc.parentNode.parentNode;
+    at.value = at.querySelectorAll('input[type="checkbox"]:checked').length;
+    console.log(at.value)
 }
+window.addEventListener( 'load', function(){
+    document.querySelectorAll(".bc input").forEach(function(e){inputs.push(e)});  
+    inputs.forEach(function(e){
+        e.onclick = changeAt;
+    });
+});
