b198be9dedeb2594515a0f762a6e57491c3980f6
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2022-03-31 20:01:58 -0300

    bug 001

--- js/atb.js
+++ js/atb.js
@@ -6,11 +6,17 @@ function changeAt(event) {
     bc.parentNode.querySelectorAll('.bc').forEach(function (e) { bcs.push(e); });
     var index = bcs.indexOf(bc);
     bcs.forEach(function (e, i) {
+        if(i === index && i >= 1){
+            console.log('i= '+i)
+            if(bcs[i-1].querySelector('input').checked === true)
+                e.querySelector('input').checked = true; 
+        }
+            
         if (i > index)
             e.querySelector('input').checked = true;
         else if (i < index)
             e.querySelector('input').checked = false;
-    });
+    });     
     var at = bc.parentNode.parentNode;
     at.value = at.querySelectorAll('input[type="checkbox"]:checked').length;
     console.log(at.value)
