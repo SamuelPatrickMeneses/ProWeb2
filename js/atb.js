ba77a9cf613bb6a40cd62aad3c6558532bb27ceb
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2022-04-14 18:15:37 -0300

    atb.js 2.0

--- js/atb.js
+++ js/atb.js
@@ -1,23 +1,41 @@
-var inputs = [];
-function changeAt(event) {
-    var src = event.currentTarget;
-    var bc = src.parentNode;
+function atValide(at){
+    var bcs = atToArray(at);
+     bcs.forEach( (e, i) => {
+         e.querySelector('input').checked = i < at.value;
+     });
+ }
+ function atToArray(at){
     var bcs = [];
-    bc.parentNode.querySelectorAll('.bc').forEach(function (e) { bcs.push(e); });
-    var index = bcs.indexOf(bc);
-    bcs.forEach(function (e, i) {
-        if (i > index)
-            e.querySelector('input').checked = true;
-        else if (i < index)
-            e.querySelector('input').checked = false;
-    });
-    var at = bc.parentNode.parentNode;
-    at.value = at.querySelectorAll('input[type="checkbox"]:checked').length;
-    console.log(at.value)
-}
-window.addEventListener( 'load', function(){
-    document.querySelectorAll(".bc input").forEach(function(e){inputs.push(e)});  
-    inputs.forEach(function(e){
-        e.onclick = changeAt;
-    });
-});
+    at.querySelectorAll('.bc').forEach(function (e) { bcs.push(e); });
+    return bcs;
+ }
+(function(){
+    function changeAt(event) {
+        var src = event.currentTarget;
+        var at = src.parentNode.parentNode.parentNode;
+        var bcs = atToArray(at);
+        var index = bcs.indexOf(src.parentNode);
+        if(src.checked)
+            at.value = index + 1;
+        else
+            if(index > 0){
+                if(bcs.length - 1 > index){
+                    console.log('case!')
+                    if(bcs[index+1].checked){
+                        src.checked = true;
+                        at.value = index + 1;
+                    }else
+                        at.value = index;  
+                }
+                                   
+            }else
+                at.value = 0;
+    } 
+    window.addEventListener( 'load', function(){
+        var inputs = [];
+        document.querySelectorAll(".bc input").forEach(function(e){inputs.push(e)});  
+        inputs.forEach(function(e){
+            e.onclick = changeAt;
+        });
+    });    
+}());
