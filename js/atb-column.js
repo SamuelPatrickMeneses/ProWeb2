7d30cc54b627b4e578aa51fad0d08ca125479263
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2022-04-18 20:14:56 -0300

    atb-culumn 1.2

--- js/atb-column.js
+++ js/atb-column.js
@@ -1,12 +1,22 @@
 (function(){
-    function ()
+    function hideCloseButton(){
+        var coms = this.parentNode.parentNode.querySelectorAll('.at5-close');
+        console.log(coms);
+        for(o of coms){
+            o.classList.toggle('displaynone');   
+        }      
+    }
+    function removeAt5(){
+        var pn = this.parentNode.parentNode;
+        pn.removeChild(this.parentNode)
+    }
     window.addEventListener('load',function(){
         var btsp = [];
-        document.querySelectorAll('.atb-column+').forEach(e => btsp.push(e));
+        document.querySelectorAll('.atb-column-mais').forEach(e => btsp.push(e));
         var btsm= [];
-        document.querySelectorAll('.atb-column-').forEach(e => btsm.push(e));
-        btsp.forEach((e) => {
-            e.parentNode.previousSibling.querySelectorAll('.at5-close')
-        });
-    })
+        document.querySelectorAll('.atb-column-menos').forEach(e => e.addEventListener('click', hideCloseButton));
+        var btsq= [];
+        document.querySelectorAll('.at5-close').forEach(e => e.addEventListener('click', removeAt5));
+
+    });
 }());
