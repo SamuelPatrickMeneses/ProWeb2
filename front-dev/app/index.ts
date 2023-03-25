c327bb8e7c3f660b83f5518c23be9e5d6465738c
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-03-25 12:18:30 -0300

    type: modify file: ./front-dev/app/index.ts description: clean file and remove depuration lines.

--- front-dev/app/index.ts
+++ front-dev/app/index.ts
@@ -1,22 +1,5 @@
-import Atb from './components/atb.js';
-import render from './util/render.js';
-import Label from './components/label.js';
-//import CloseButton from './components/closerButton.js';
-//import Item from './components/item.js';
-//import ItemListView from './components/itemListView.js';
-//import Column from './components/column.js';
-//import login  from './controller/login-e-cadastro.js';
+import loginECadastro from './controller/login-e-cadastro.js';
 
-//import LifeBox from './components/box.js';
-//import ItemGridView from './components/itemGridView.js';
-//import Ficha from './components/ficha.js';
-//import ListItem from './components/listItem.js';
-//import lista from './controller/lista.js';
-//import Component from './components/component.js';
-import Bol from './components/bol.js';
-import AtbLimit from './components/atbLimit.js';
-// import html from '../index.html';
-//import style from '../assets/css/dev.css';
 window.onload = () => {
     'use strict';
     let root  = <HTMLElement> document.getElementById('root');
@@ -140,11 +123,6 @@ window.onload = () => {
             type:'text'
         },
     ]}    ));*/
-    //login();
-    const bol  = render(AtbLimit,{value:3,size:5});
-    const box  = render(Atb,{value:3,size:5,box:true});
-    root.appendChild(bol);
-    root.appendChild(box);
-    root.appendChild(render(Label,{value:'label'}));
+    loginECadastro();
 };
 export default {};
