ab900624f106090ec141a5195d4fb36a11ec1114
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-03-25 12:09:36 -0300

    type: modify file: ./front-dev/app/util/base64.ts description: removo url encode and decode.

--- front-dev/app/util/base64.ts
+++ front-dev/app/util/base64.ts
@@ -1,11 +1,11 @@
 
 const base64 = {
   utf8_to_b64(str :string) {
-    return window.btoa(encodeURIComponent(str));
+    return window.btoa(str);
   },
   
   b64_to_utf8(str :string) {
-    return decodeURIComponent(window.atob(str));
+    return window.atob(str);
   }
 }
 export default base64;
