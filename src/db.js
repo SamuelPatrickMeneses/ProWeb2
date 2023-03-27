7d4b72b0bde8f554a92b0fbfb9e114199b42d917
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-03-27 13:47:30 -0300

    type: modify file: ./src/db.js description: add pressistense configuration through enviroment variables.

--- src/db.js
+++ src/db.js
@@ -1,10 +1,18 @@
 'use strict';
 const Cliente = require('mongodb').MongoClient;
 
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASSWORD;
+const db   = process.env.DB || 'storyteller';
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+
+
 module.exports = async function(){
+    const url = `mongodb://${user}:${pass}@${host}:${port}/${db}`;
     try{
-        const com = await Cliente.connect('mongodb://localhost:27017');
-        return com.db('storyteller');
+        const com = await Cliente.connect(url);
+        return com.db(db);
     }catch(err){
         throw err;
     }
